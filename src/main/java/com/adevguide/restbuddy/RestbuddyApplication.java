115aa17c184c3eead947b1f82682ff0c9f4b57c9
pbhuite <prabhu.sites@gmail.com>
2019-09-20 18:10:15 +0530

    Initial commit

---
+++ src/main/java/com/adevguide/restbuddy/RestbuddyApplication.java
@@ -0,0 +1,13 @@
+package com.adevguide.restbuddy;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class RestbuddyApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(RestbuddyApplication.class, args);
+	}
+
+}
