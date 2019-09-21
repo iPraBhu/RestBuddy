24dd7b5afe271c5bee21946ff421ca49f0a68ac4
pbhuite <prabhu.sites@gmail.com>
2019-09-21 23:19:06 +0530

    added backend

--- src/main/java/com/adevguide/restbuddy/RestBuddyConfiguration.java
+++ src/main/java/com/adevguide/restbuddy/RestBuddyConfiguration.java
@@ -14,7 +14,8 @@ import org.springframework.web.servlet.config.annotation.WebMvcConfigurer;
 public class RestBuddyConfiguration implements WebMvcConfigurer{
     @Override
     public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**");
+        registry.addMapping("/**")
+        .allowedMethods("*");
     }
 }
 
--- ui/js/main.js
+++ ui/js/main.js
@@ -1,154 +1,251 @@
-var host="https://restbuddy.herokuapp.com/";
+var host;
+if (location.pathname.includes('github')) {
+    host = "https://restbuddy.herokuapp.com/";
+} else {
+    host = "http://localhost:7777/";
+}
 
-(function ($) {
-  "use strict";
 
-  // Preloader (if the #preloader div exists)
-  $(window).on('load', function () {
-    if ($('#preloader').length) {
-      $('#preloader').delay(100).fadeOut('slow', function () {
-        $(this).remove();
-      });
-    }
-  });
+(function($) {
+    "use strict";
 
-  // Back to top button
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('.back-to-top').fadeIn('slow');
-    } else {
-      $('.back-to-top').fadeOut('slow');
-    }
-  });
-  $('.back-to-top').click(function(){
-    $('html, body').animate({scrollTop : 0},1500, 'easeInOutExpo');
-    return false;
-  });
-
-  // Initiate the wowjs animation library
-  new WOW().init();
-
-  // Header scroll class
-  $(window).scroll(function() {
-    if ($(this).scrollTop() > 100) {
-      $('#header').addClass('header-scrolled');
-    } else {
-      $('#header').removeClass('header-scrolled');
-    }
-  });
-
-  if ($(window).scrollTop() > 100) {
-    $('#header').addClass('header-scrolled');
-  }
-
-  // Smooth scroll for the navigation and links with .scrollto classes
-  $('.main-nav a, .mobile-nav a, .scrollto').on('click', function() {
-    if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
-      var target = $(this.hash);
-      if (target.length) {
-        var top_space = 0;
-
-        if ($('#header').length) {
-          top_space = $('#header').outerHeight();
-
-          if (! $('#header').hasClass('header-scrolled')) {
-            top_space = top_space - 40;
-          }
+    // Preloader (if the #preloader div exists)
+    $(window).on('load', function() {
+        if ($('#preloader').length) {
+            $('#preloader').delay(100).fadeOut('slow', function() {
+                $(this).remove();
+            });
         }
+    });
 
-        $('html, body').animate({
-          scrollTop: target.offset().top - top_space
-        }, 1500, 'easeInOutExpo');
-
-        if ($(this).parents('.main-nav, .mobile-nav').length) {
-          $('.main-nav .active, .mobile-nav .active').removeClass('active');
-          $(this).closest('li').addClass('active');
+    // Back to top button
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('.back-to-top').fadeIn('slow');
+        } else {
+            $('.back-to-top').fadeOut('slow');
         }
-
-        if ($('body').hasClass('mobile-nav-active')) {
-          $('body').removeClass('mobile-nav-active');
-          $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
-          $('.mobile-nav-overly').fadeOut();
-        }
+    });
+    $('.back-to-top').click(function() {
+        $('html, body').animate({ scrollTop: 0 }, 1500, 'easeInOutExpo');
         return false;
-      }
+    });
+
+    // Initiate the wowjs animation library
+    new WOW().init();
+
+    // Header scroll class
+    $(window).scroll(function() {
+        if ($(this).scrollTop() > 100) {
+            $('#header').addClass('header-scrolled');
+        } else {
+            $('#header').removeClass('header-scrolled');
+        }
+    });
+
+    if ($(window).scrollTop() > 100) {
+        $('#header').addClass('header-scrolled');
     }
-  });
 
-  // Navigation active state on scroll
-  var nav_sections = $('section');
-  var main_nav = $('.main-nav, .mobile-nav');
-  var main_nav_height = $('#header').outerHeight();
+    // Smooth scroll for the navigation and links with .scrollto classes
+    $('.main-nav a, .mobile-nav a, .scrollto').on('click', function() {
+        if (location.pathname.replace(/^\//, '') == this.pathname.replace(/^\//, '') && location.hostname == this.hostname) {
+            var target = $(this.hash);
+            if (target.length) {
+                var top_space = 0;
 
-  $(window).on('scroll', function () {
-    var cur_pos = $(this).scrollTop();
-  
-    nav_sections.each(function() {
-      var top = $(this).offset().top - main_nav_height,
-          bottom = top + $(this).outerHeight();
-  
-      if (cur_pos >= top && cur_pos <= bottom) {
-        main_nav.find('li').removeClass('active');
-        main_nav.find('a[href="#'+$(this).attr('id')+'"]').parent('li').addClass('active');
-      }
+                if ($('#header').length) {
+                    top_space = $('#header').outerHeight();
+
+                    if (!$('#header').hasClass('header-scrolled')) {
+                        top_space = top_space - 40;
+                    }
+                }
+
+                $('html, body').animate({
+                    scrollTop: target.offset().top - top_space
+                }, 1500, 'easeInOutExpo');
+
+                if ($(this).parents('.main-nav, .mobile-nav').length) {
+                    $('.main-nav .active, .mobile-nav .active').removeClass('active');
+                    $(this).closest('li').addClass('active');
+                }
+
+                if ($('body').hasClass('mobile-nav-active')) {
+                    $('body').removeClass('mobile-nav-active');
+                    $('.mobile-nav-toggle i').toggleClass('fa-times fa-bars');
+                    $('.mobile-nav-overly').fadeOut();
+                }
+                return false;
+            }
+        }
     });
-  });
 
-  // jQuery counterUp (used in Whu Us section)
-  $('[data-toggle="counter-up"]').counterUp({
-    delay: 10,
-    time: 1000
-  });
+    // Navigation active state on scroll
+    var nav_sections = $('section');
+    var main_nav = $('.main-nav, .mobile-nav');
+    var main_nav_height = $('#header').outerHeight();
 
-  // Porfolio isotope and filter
-  $(window).on('load', function () {
-    var portfolioIsotope = $('.portfolio-container').isotope({
-      itemSelector: '.portfolio-item'
+    $(window).on('scroll', function() {
+        var cur_pos = $(this).scrollTop();
+
+        nav_sections.each(function() {
+            var top = $(this).offset().top - main_nav_height,
+                bottom = top + $(this).outerHeight();
+
+            if (cur_pos >= top && cur_pos <= bottom) {
+                main_nav.find('li').removeClass('active');
+                main_nav.find('a[href="#' + $(this).attr('id') + '"]').parent('li').addClass('active');
+            }
+        });
     });
-    $('#portfolio-flters li').on( 'click', function() {
-      $("#portfolio-flters li").removeClass('filter-active');
-      $(this).addClass('filter-active');
-  
-      portfolioIsotope.isotope({ filter: $(this).data('filter') });
+
+    // jQuery counterUp (used in Whu Us section)
+    $('[data-toggle="counter-up"]').counterUp({
+        delay: 10,
+        time: 1000
     });
-  });
 
-  // Testimonials carousel (uses the Owl Carousel library)
-  $(".testimonials-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    items: 1
-  });
+    // Porfolio isotope and filter
+    $(window).on('load', function() {
+        var portfolioIsotope = $('.portfolio-container').isotope({
+            itemSelector: '.portfolio-item'
+        });
+        $('#portfolio-flters li').on('click', function() {
+            $("#portfolio-flters li").removeClass('filter-active');
+            $(this).addClass('filter-active');
 
-  // Clients carousel (uses the Owl Carousel library)
-  $(".clients-carousel").owlCarousel({
-    autoplay: true,
-    dots: true,
-    loop: true,
-    responsive: { 0: { items: 2 }, 768: { items: 4 }, 900: { items: 6 }
-    }
-  });
+            portfolioIsotope.isotope({ filter: $(this).data('filter') });
+        });
+    });
+
+    // Testimonials carousel (uses the Owl Carousel library)
+    $(".testimonials-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        items: 1
+    });
+
+    // Clients carousel (uses the Owl Carousel library)
+    $(".clients-carousel").owlCarousel({
+        autoplay: true,
+        dots: true,
+        loop: true,
+        responsive: {
+            0: { items: 2 },
+            768: { items: 4 },
+            900: { items: 6 }
+        }
+    });
 
 })(jQuery);
 
 
-$('#api1').click(function(){
-  var api=host+"city/allcities";
 
-  $('#requestEndpointID').html(`<a href="${api}" target="_blank">${api}</a>`);
+function randomNumberFromRange(min, max) {
+    return Math.floor(Math.random() * (max - min + 1) + min);
+}
 
-  axios.get(api)
-  .then(function (response) {
-    // handle success
-    console.log(response);
-  })
-  .catch(function (error) {
-    // handle error
-    console.log(error);
-  })
-  .finally(function () {
-    // always executed
-  });
+// All Cities
+$('#api1').click(function() {
+    var api = host + "city/allcities";
+
+    $('#requestEndpointID').html(`<a href="${api}" target="_blank">${api}</a>`);
+
+    axios.get(api)
+        .then(function(response) {
+            // handle success
+            $('#responseStatusID').html(response.status);
+            $('#responseDataID').html('<pre>' + JSON.stringify(response.data, null, 2) + '</pre>');
+            console.log(response);
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function() {
+            // always executed
+        });
+
+});
+
+
+// City by ID
+$('#api2').click(function() {
+    var randomNum = randomNumberFromRange(1, 1300);
+    var api = host + "city/" + randomNum;
+
+    $('#requestEndpointID').html(`<a href="${api}" target="_blank">${api}</a>`);
+
+    axios.get(api)
+        .then(function(response) {
+            // handle success
+            $('#responseStatusID').html(response.status);
+            $('#responseDataID').html('<pre>' + JSON.stringify(response.data, null, 2) + '</pre>');
+            console.log(response);
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function() {
+            // always executed
+        });
+
+});
+
+
+// Add new city
+$('#api3').click(function() {
+    //var randomNum = randomNumberFromRange(1, 1300);
+    var api = host + "city/addcity";
+
+    $('#requestEndpointID').html(`<a href="${api}" target="_blank">${api}</a>`);
+
+    axios.post(api, {
+            cityname: "new city",
+            statecode: "NC",
+            statename: "new state",
+            zipcode: "04546"
+        })
+        .then(function(response) {
+            // handle success
+            $('#responseStatusID').html(response.status);
+            $('#responseDataID').html('<pre>' + JSON.stringify(response.data, null, 2) + '</pre>');
+            console.log(response);
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function() {
+            // always executed
+        });
+
+});
+
+// Delete a city
+$('#api4').click(function() {
+    var randomNum = randomNumberFromRange(1, 1300);
+    var api = host + "city/" + randomNum;
+
+
+    $('#requestEndpointID').html(`<a href="${api}" target="_blank">${api}</a>`);
+
+    axios.delete(api)
+        .then(function(response) {
+            // handle success
+            $('#responseStatusID').html(response.status);
+            $('#responseDataID').html(response.data);
+            console.log(response);
+        })
+        .catch(function(error) {
+            // handle error
+            console.log(error);
+        })
+        .finally(function() {
+            // always executed
+        });
 
 });
