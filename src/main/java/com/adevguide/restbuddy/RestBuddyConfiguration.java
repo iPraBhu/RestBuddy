042bae2d741c197028381086e753af073cf77051
unknown <pratik.bhuite@gmail.com>
2021-05-16 17:01:50 +0530

    refactoring

--- src/main/java/com/adevguide/restbuddy/RestBuddyConfiguration.java
+++ src/main/java/com/adevguide/restbuddy/RestBuddyConfiguration.java
@@ -2,12 +2,19 @@ package com.adevguide.restbuddy;
 
 import static springfox.documentation.builders.PathSelectors.regex;
 
+import java.time.LocalDateTime;
+
+import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.scheduling.annotation.Scheduled;
 import org.springframework.web.servlet.config.annotation.CorsRegistry;
 import org.springframework.web.servlet.config.annotation.ResourceHandlerRegistry;
 import org.springframework.web.servlet.config.annotation.WebMvcConfigurationSupport;
 
+import com.adevguide.restbuddy.service.SelfFeignService;
+
+import lombok.extern.slf4j.Slf4j;
 import springfox.documentation.builders.ApiInfoBuilder;
 import springfox.documentation.builders.RequestHandlerSelectors;
 import springfox.documentation.service.ApiInfo;
@@ -22,40 +29,45 @@ import springfox.documentation.swagger2.annotations.EnableSwagger2;
  */
 @Configuration
 @EnableSwagger2
+@Slf4j
 public class RestBuddyConfiguration extends WebMvcConfigurationSupport {
 
-    @Override
-    public void addCorsMappings(CorsRegistry registry) {
-        registry.addMapping("/**").allowedMethods("*");
-    }
+	@Autowired
+	SelfFeignService selfFeign;
 
-    /* Swagger Config starts */
-    @Override
-    protected void addResourceHandlers(ResourceHandlerRegistry registry) {
-        registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
+	@Override
+	public void addCorsMappings(CorsRegistry registry) {
+		registry.addMapping("/**").allowedMethods("*");
+	}
 
-        registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
-    }
-    
-    
-    private ApiInfo metaData() {
-        return new ApiInfoBuilder()
-                .title("RestBuddy API Documentation")
-                .description("RestBuddy Documentation for all exposed endpoints")
-                .version("1.0.0")
-                .license("Apache License Version 2.0")
-                .contact(new Contact("PraBhu", "https://www.adevguide.com/contact-us", "prabhu.sites@gmail.com"))
-                .build();
-    }
+	/* Swagger Config starts */
+	@Override
+	protected void addResourceHandlers(ResourceHandlerRegistry registry) {
+		registry.addResourceHandler("swagger-ui.html").addResourceLocations("classpath:/META-INF/resources/");
 
-    @Bean
-    public Docket cityApi() {
-        return new Docket(DocumentationType.SWAGGER_2).select()
-                .apis(RequestHandlerSelectors.basePackage("com.adevguide.restbuddy.controller")).paths(regex("/city.*"))
-                .build().apiInfo(metaData());
+		registry.addResourceHandler("/webjars/**").addResourceLocations("classpath:/META-INF/resources/webjars/");
+	}
 
-    }
-    
-    
-    
+	private ApiInfo metaData() {
+		return new ApiInfoBuilder().title("RestBuddy API Documentation")
+				.description("RestBuddy Documentation for all exposed endpoints").version("1.0.0")
+				.license("Apache License Version 2.0")
+				.contact(new Contact("PraBhu", "https://www.adevguide.com/contact-us", "prabhu.sites@gmail.com"))
+				.build();
+	}
+
+	@Bean
+	public Docket cityApi() {
+		return new Docket(DocumentationType.SWAGGER_2).select()
+				.apis(RequestHandlerSelectors.basePackage("com.adevguide.restbuddy.controller")).paths(regex("/city.*"))
+				.build().apiInfo(metaData());
+
+	}
+
+	@Scheduled(cron = "* */25 * * * *")
+	public void selfHit() {
+		log.info("Self hitting at {}", LocalDateTime.now());
+		selfFeign.selfHit();
+	}
+
 }
